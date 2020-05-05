9f4dc0fabaae1ff9b0dcebae7b02c5f9ee4fdb2b
Edws <eduardos.rocha17@gmail.com>
2020-05-05 13:57:07 -0300

    Basic Calendar
    Generates a Calendar without styles or details

---
+++ client/calendar.js
@@ -0,0 +1,74 @@
+/* Leonardo Moreira Kobe    NºUSP: 9778623
+Rafael Tavares Oliveira          NºUSP: 11219071
+Eduardo Souza Rocha           NºUSP: 11218692 */
+
+/**
+ * Location is the HTML tag where the calendar will be rendered
+ */
+export default class Calendar {
+   constructor(location) {
+      this.location = location
+      this.location.innerHTML = ''
+      this.now = new Date()
+      this.location.append(this.createCalendar())
+   }
+
+   /**
+    * Create a Calendar table
+    */
+   createCalendar() {
+      const start = new Date(this.now.getFullYear(), this.now.getMonth(), 1)
+      const end = new Date(this.now.getFullYear(), this.now.getMonth() + 1, 0)
+
+      const { table, tbody } = newTable()
+
+      let day = 1
+
+      let tr = newTR()
+      for (let i = 0; i < 6; i++) {
+         if (i < start.getDay()) tr.appendChild(newTD('*'))
+         else tr.appendChild(newTD(day++))
+      }
+      tr.appendChild(newTD(day++), true)
+      tbody.appendChild(tr)
+
+      while (day <= end.getDate() - 7) {
+         let tr = newTR()
+         for (let i = 0; i < 6; i++) {
+            tr.appendChild(newTD(day++))
+         }
+         tr.appendChild(newTD(day++), true)
+         tbody.appendChild(tr)
+
+      }
+
+      tr = newTR()
+      for (let i = 0; i < 6; i++) {
+         if (day <= end.getDate()) tr.appendChild(newTD(day++))
+         else tr.appendChild(newTD('*'))
+      }
+      if (day <= end.getDate()) tr.appendChild(newTD(day++), true)
+      else tr.appendChild(newTD('*'))
+      tbody.appendChild(tr)
+
+      return table
+   }
+}
+
+function newTable() {
+   let table = document.createElement('table')
+   let tbody = document.createElement('tbody')
+   table.appendChild(tbody)
+   return { table, tbody }
+}
+
+function newTD(day, sunday = false) {
+   let td = document.createElement('td')
+   td.innerText = day
+   if (sunday) td.className = 'calendar-sunday'
+   return td
+}
+
+function newTR() {
+   return document.createElement('tr')
+}
